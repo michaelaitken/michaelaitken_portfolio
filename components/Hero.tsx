f94ebe0716e651ae8a004d4dd03e61c9929773fb
Michael Aitken <mike@mikeaitken.net>
2023-02-07 18:24:27 +0800

    Finished the Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import Image from 'next/image';
 import { Roboto_Condensed, Roboto } from '@next/font/google';
+import downArrow from '../public/down_arrow.svg';
+import github from '../public/github.svg';
+import linkedin from '../public/linkedin.svg';
 
 type Props = {}
 
@@ -16,15 +20,55 @@ const roboto = Roboto({
 })
 
 
+
 const Hero = ({}: Props) => {
+  const socials = [
+  {
+    name: 'github',
+    image: github,
+    link: 'www.github.com'
+  },
+  {
+    name: 'linkedin',
+    image: linkedin,
+    link: 'www.linkedin.com'
+  },
+];
+
   return (
-    <div className='flex flex-col justify-center items-center w-[100vw] h-[80vh] -mt-[80px]'>
-      <div className='flex flex-col xl:flex-row items-center'>
-        <h1 className={`text-[90px] leading-[90px] md:text-[210px] md:leading-[210px] lg:text-[230px] lg:leading-[230px] text-white font-semibold uppercase ${robotoCondensed.className} xl:mr-[80px] animate-h1Animation`}>michael</h1>
-        <h1 className={`text-[90px] leading-[90px] md:text-[210px] md:leading-[210px] lg:text-[230px] lg:leading-[230px] text-white font-semibold uppercase ${robotoCondensed.className} animate-h1Animation`}>aitken</h1>
+    <div className='flex flex-col justify-between items-center'>
+      <div className='flex flex-col justify-center items-center w-[100vw] h-[80vh] -mt-[80px]'>
+        <div className='flex flex-col xl:flex-row items-center'>
+          <h1 className={`text-[90px] leading-[90px] md:text-[210px] md:leading-[210px] lg:text-[230px] lg:leading-[230px] text-white font-semibold uppercase ${robotoCondensed.className} xl:mr-[80px] animate-h1Animation`}>michael</h1>
+          <h1 className={`text-[90px] leading-[90px] md:text-[210px] md:leading-[210px] lg:text-[230px] lg:leading-[230px] text-white font-semibold uppercase ${robotoCondensed.className} animate-h1Animation`}>aitken</h1>
+        </div>
+        <p className={`text-[18px] md:text-[28px] lg:text-[32px] text-[#FF8A00] font-semibold uppercase md:-mt-1 ${roboto.className}`}>software developer</p>
       </div>
-      <p className={`text-[18px] md:text-[28px] lg:text-[32px] text-[#FF8A00] font-semibold uppercase md:-mt-1 ${roboto.className}`}>software developer</p>
+      <div className='flex justify-between absolute w-[100vw] bottom-10 z-10 max-w-[1920px]'>
+        <div className='w-[40px] sm:w-[160px]'/>
+        <div className='flex justify-end'>
+          <a href='#projects'>
+            <Image src={downArrow} alt="arrow" className='object-contain animate-bounce' />
+          </a>
+        </div>
+        <div className='flex flex-col sm:flex-row items-end sm:items-center mr-10'>
+          {socials.map((social, i) => (
+            <a 
+              key={i}  
+              href={social.link}
+              className="my-5 sm:mx-5"
+            >
+              <Image
+              src={social.image}
+              alt={social.name}
+              className="w-[40px] h-[40px] fill-slate-500"
+              />
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
+    
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,16 +26,16 @@ export default function Navbar({}: Props) {
             {/* Navbar for normal screen size ${(0.75+(i*0.5)).toString} */}
             <div className="flex justify-between items-center max-w-[2000px] mx-auto">
                 <div className="ml-[60px] lg:ml-[100px] mt-[50px]">
-                    <Image className="w-[100px] h-[100px] z-10 object-contain" src={logo} alt="logo"/>
+                    <Image className="w-[100px] h-[100px] object-contain" src={logo} alt="logo"/>
                 </div>
-                <div className="">
+                <div className="z-10">
                     <ul className="hidden lg:flex flex-row text-white text-[32px] mr-[90px]"> 
                         {links.map((link, i) => (
                             <li 
                             key={i}
-                            className={`mx-[40px] mt-7 font-thin hover:text-[#FF8A00] animate-[linksAnimation_0.75_ease-in-out]`}
+                            className={"mx-[40px] mt-7 font-thin hover:text-[#FF8A00] uppercase"}
                             >
-                            <a href={`#${link}`}>{link.toUpperCase()}</a>
+                                <a href={`#${link}`}>{link}</a>
                             </li>
                         ))}
                     </ul>
@@ -44,7 +44,7 @@ export default function Navbar({}: Props) {
                 {/* Navbar Menu for Mobile */}
                 <div className="relative flex lg:hidden justify-end">
                     <button 
-                        className="mt-[50px] mr-[60px] z-10"
+                        className="mt-[50px] mr-[60px] z-50"
                         onClick={handleToggle}
                     >
                         {menuOpen ? (
@@ -53,7 +53,7 @@ export default function Navbar({}: Props) {
                             <Image className="w-[80px] h-[80px] object-contain" src={menu} alt="menu"/>
                         )}
                     </button>
-                    <div className={`bg-gradient-to-r from-[#000000]/95 to-transparent backdrop-blur-md w-[100vw] h-[100vh] t-0 l-0 ${menuOpen ? "fixed overflow-y-scroll hide-scroll-bar" : "hidden"}`}>
+                    <div className={`bg-gradient-to-r from-[#000000]/95 to-transparent backdrop-blur-md w-[100vw] h-[100vh] t-0 l-0 z-40 ${menuOpen ? "fixed overflow-y-scroll hide-scroll-bar" : "hidden"}`}>
                         <div className="flex flex-col">
                             <ul className="text-white text-[50px] ml-[70px] mt-[100px]">
                                 {links.map((link, i) => (
