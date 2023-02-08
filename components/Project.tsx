0bea0b7b49012319ec72e0efe5e42ff1da5bfbfc
Michael Aitken <mike@mikeaitken.net>
2023-02-08 14:40:57 +0800

    Preparing for Project Cards

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,8 +4,16 @@ type Props = {}
 
 const Project = (props: Props) => {
   return (
-    <div className='text-white'>
-        Project
+    <div className='text-white px-6 py-12'>
+      <div className='max-w-[1920px] mx-auto w-fullflex flex-col'>
+        <h1 className='text-center uppercase text-[60px]'>Project</h1>
+        <p className='text-center text-white'>Information Here</p>
+        <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'>
+          {/* <ProjectCard 
+            key={project.key}
+          /> */}
+        </div>
+      </div>
     </div>
   )
 }
